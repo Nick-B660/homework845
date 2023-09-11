ce184282a9e5d771137dd2d9f158b21bacda7724
Nick-B660 <nick.anon.b@gmail.com>
2023-09-11 23:43:06 +0200

    test

--- save_json.js
+++ save_json.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
-const save = (favNumber) => {
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+
+await s3.putObject({
+  Body: JSON.stringify(favNumber, null, 2),
+  Bucket: "cyclic-perfect-erin-bat-eu-west-2",
+  Key: "some_files/my_file.json",
+}).promise()
+
+
 };
 
 module.exports = { save };
